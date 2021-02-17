a1e9b02329521a7e172d3d36c9db2453af225448
Tom Bonynge <tombonynge@gmail.com>
2021-02-17 16:32:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Typescript-Webpack setup
+
+Provides a barebones typescript setup with webpack.
+
+## Instructions
+
+1. Run npm install
+2. npm link typescript (I have to do this).
+
+## Run
+
+1. npm run build
+2. npm run build:watch
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "ts-loader",
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+};
